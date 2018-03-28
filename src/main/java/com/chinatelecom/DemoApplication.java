46a59c1b6eb4ed940792dfb41ba51d31fc0f5c02
wu <wuguojun@chinatelecom.cn>
2018-03-28 19:00:17 +0800

    fix pom

--- src/main/java/com/chinatelecom/DemoApplication.java
+++ src/main/java/com/chinatelecom/DemoApplication.java
@@ -6,9 +6,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"cn.chinatelecom.dp.*"})
+@PropertySource(value={"classpath:conf/sdk.properties","application.properties"})
 public class DemoApplication {
 
 
